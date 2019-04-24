b1f238b94c2b55e8527ab48a172106f00bb4ef1c
okendoken <philip@flatlogic.com>
2019-04-24 17:06:29 +0300

    fix deploy

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-material-template/precache-manifest.e23f025ae7a3e6877bf22a4d4933fcc6.js"
+  "/react-material-admin/precache-manifest.e23f025ae7a3e6877bf22a4d4933fcc6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-material-template/index.html", {
+workbox.routing.registerNavigationRoute("/react-material-admin/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
