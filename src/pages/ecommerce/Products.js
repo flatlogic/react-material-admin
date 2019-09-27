546586c450b88a8c48314ca294916152cd5c7fbc
Michael Daineka <michaeldaineka@gmail.com>
2019-09-27 18:33:44 +0300

    feat: product manage init, some other improvements

---
+++ src/pages/ecommerce/Products.js
@@ -0,0 +1,118 @@
+import React from "react";
+import img1 from '../../images/img1.jpg'
+import img2 from '../../images/img2.jpg'
+import img3 from '../../images/img3.jpg'
+import img4 from '../../images/img4.jpg'
+import img5 from '../../images/img5.jpeg'
+import img6 from '../../images/img6.jpg'
+
+
+export const rows = [
+  {
+    id: 0,
+    img: img1,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 1,
+    img: img2,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 2,
+    img: img3,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 3,
+    img: img4,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 4,
+    img: img5,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 5,
+    img: img6,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 6,
+    img: img1,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 7,
+    img: img2,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 8,
+    img: img3,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 9,
+    img: img4,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 10,
+    img: img5,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  },
+  {
+    id: 11,
+    img: img6,
+    title: 'Trainers',
+    subtitle: 'Trainers In White',
+    price: 76,
+    rating: 4.6,
+  }
+]
+
+
+const Product = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Product;
---
+++ src/pages/ecommerce/styles.js
@@ -0,0 +1,17 @@
+import { makeStyles } from '@material-ui/styles'
+
+export default makeStyles(theme => ({
+  table: {
+    minWidth: 500,
+    '& .MuiTableRow-root:nth-child(odd)': {
+      backgroundColor: '#f3f3f3'
+    },
+  },
+  tableFooter: {
+    backgroundColor: '#fff !important'
+  },
+  tableWrapper: {
+    marginTop: theme.spacing(1),
+    overflowX: 'auto',
+  },
+}))
