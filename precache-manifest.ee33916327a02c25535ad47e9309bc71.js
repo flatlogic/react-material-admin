1f86c3fa5e564650706270fda06f86ac7c6ea99c
Michael Daineka <michaeldaineka@gmail.com>
2019-11-21 19:10:46 +0300

    fix: abandoned files add

---
+++ precache-manifest.ee33916327a02c25535ad47e9309bc71.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f103b57dfd175c5e9c74f1b6b1513b56",
+    "url": "/react-material-admin/index.html"
+  },
+  {
+    "revision": "e19f555304d2ee5e92e1",
+    "url": "/react-material-admin/static/css/2.b95fcbb1.chunk.css"
+  },
+  {
+    "revision": "e19f555304d2ee5e92e1",
+    "url": "/react-material-admin/static/js/2.d586ddca.chunk.js"
+  },
+  {
+    "revision": "81dc03b6dc347bc83a0e",
+    "url": "/react-material-admin/static/js/main.a1bdcd63.chunk.js"
+  },
+  {
+    "revision": "f720ee1dec0f84303a39",
+    "url": "/react-material-admin/static/js/runtime~main.cad08fbd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-material-admin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-material-admin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-material-admin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-material-admin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-material-admin/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "695a31606b1abb167b03fb1f6375831d",
+    "url": "/react-material-admin/static/media/google.695a3160.svg"
+  },
+  {
+    "revision": "a0185b043dee68a72aebffdcc939e21e",
+    "url": "/react-material-admin/static/media/logo.a0185b04.svg"
+  }
+]);
