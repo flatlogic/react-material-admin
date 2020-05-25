33713d23d0f212a1021212dd21598ae0003f510b
Vlad <excelent91@mail.ru>
2020-05-25 16:44:57 +0300

    updated BE url

--- src/config.js
+++ src/config.js
@@ -1,15 +1,15 @@
 const hostApi =
   process.env.NODE_ENV === "development"
     ? "http://localhost"
-    : "https://flatlogic-node-backend.herokuapp.com";
+    : "https://sing-generator-node.herokuapp.com";
 const portApi = process.env.NODE_ENV === "development" ? 8080 : "";
-const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}`;
+const baseURLApi = `${hostApi}${portApi ? `:${portApi}/api` : ``}`;
 
 export default {
   hostApi,
   portApi,
   baseURLApi,
-  remote: "https://flatlogic-node-backend.herokuapp.com",
+  remote: "https://sing-generator-node.herokuapp.com",
   isBackend: process.env.REACT_APP_BACKEND,
   auth: {
     email: "admin@flatlogic.com",
