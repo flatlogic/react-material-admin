19484da3bff33610fcf16b61e9e9cd03560b3703
Vlad <excelent91@mail.ru>
2020-06-05 13:43:10 +0300

    fix url typo

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const hostApi = process.env.NODE_ENV === "development"
     ? "http://localhost"
     : "https://sing-generator-node.herokuapp.com";
 const portApi = process.env.NODE_ENV === "development" ? 8080 : "";
-const baseURLApi = `${hostApi}${portApi ? `:${portApi}/api` : ``}`;
+const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}/api`;
 const redirectUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://flatlogic.github.io/react-material-admin-full";
 
 export default {
